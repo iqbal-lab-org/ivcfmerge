abd3176dc0f6c0f16495592defb2ea883f1d28f3
Giang Nguyen <giang@ebi.ac.uk>
2020-02-11 10:15:27 +0000

    Multi VCF reader should implement the context manager protocol

---
+++ multifile/reader.py
@@ -0,0 +1,9 @@
+class MultiVCFReader:
+    def __init__(self, input_paths):
+        pass
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *args, **kwargs):
+        pass
---
+++ tests/test_multi_vcf_reader.py
@@ -0,0 +1,28 @@
+import os
+import pytest
+
+from multifile.reader import MultiVCFReader
+
+
+@pytest.fixture
+def ref_data():
+    this_script_dir = os.path.dirname(os.path.realpath(__file__))
+    test_data_dir = os.path.join(this_script_dir, 'data')
+
+    input_filenames = ['ref_%d.vcf' % i for i in range(6)]
+
+    return {
+        'input_paths': [os.path.join(test_data_dir, filename) for filename in input_filenames]
+    }
+
+
+def test_reader_implements_context_manager_protocol(ref_data):
+    with MultiVCFReader(ref_data['input_paths']) as reader:
+        pass
+
+
+def test_reader_forces_using_context_manager():
+    pass
+
+
+
