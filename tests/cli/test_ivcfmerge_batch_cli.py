c2cc40e0feb46e28620bf24d300686e45532f82f
Giang Nguyen <giang@ebi.ac.uk>
2020-02-28 10:30:39 +0000

    Explicitly use python3 in test cmd

--- tests/cli/test_ivcfmerge_batch_cli.py
+++ tests/cli/test_ivcfmerge_batch_cli.py
@@ -6,7 +6,7 @@ from hypothesis import given, strategies as st
 def test_merging_example_input(input_paths_file, ref_merged_path, tmp_path):
     output_path = tmp_path / 'out.vcf'
 
-    subprocess.run(['python', 'ivcfmerge_batch.py', input_paths_file, str(output_path)])
+    subprocess.run(['python3', 'ivcfmerge_batch.py', input_paths_file, str(output_path)])
 
     with output_path.open() as outfile, open(ref_merged_path, 'r') as ref_merged:
         assert outfile.read() == ref_merged.read()
@@ -16,7 +16,7 @@ def test_merging_example_input(input_paths_file, ref_merged_path, tmp_path):
 def test_batch_sizes(input_paths_file, ref_merged_path, tmp_path, batch_size):
     output_path = tmp_path / 'out.vcf'
 
-    subprocess.run(['python', 'ivcfmerge_batch.py', '--batch-size', str(batch_size), input_paths_file,
+    subprocess.run(['python3', 'ivcfmerge_batch.py', '--batch-size', str(batch_size), input_paths_file,
                     str(output_path)])
 
     with output_path.open() as outfile, open(ref_merged_path, 'r') as ref_merged:
@@ -27,7 +27,7 @@ def test_batch_sizes(input_paths_file, ref_merged_path, tmp_path, batch_size):
 def test_temporary_dir(input_paths_file, ref_merged_path, tmp_path, batch_size):
     output_path = tmp_path / 'out.vcf'
 
-    subprocess.run(['python', 'ivcfmerge_batch.py', '--batch-size', str(batch_size),
+    subprocess.run(['python3', 'ivcfmerge_batch.py', '--batch-size', str(batch_size),
                     '--temp-dir', str(tmp_path), input_paths_file, str(output_path)])
 
     with output_path.open() as outfile, open(ref_merged_path, 'r') as ref_merged:
