d5c14f1c7af9be90fb1859415f1f45d577a5450a
Giang Nguyen <giang@ebi.ac.uk>
2020-04-08 15:19:22 +0100

    Feature/install scripts (#14)
    * Add install scripts
    
    * Readme

---
+++ ivcfmerge/cli/batch.py
@@ -0,0 +1,16 @@
+import argparse
+
+from ivcfmerge import ivcfmerge_batch
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch-size', type=int)
+    parser.add_argument('--temp-dir', type=str)
+    parser.add_argument('input_paths_file', type=str)
+    parser.add_argument('output_path', type=str)
+    args = parser.parse_args()
+
+    with open(args.input_paths_file, 'r') as input_paths_file:
+        input_paths = input_paths_file.read().splitlines()
+        ivcfmerge_batch(input_paths, args.output_path, args.batch_size, args.temp_dir)
